bfc54767f4701b5446e886a84f4145d68cd68f07
David Monllao <david.monllao@gmail.com>
2019-01-08 19:31:34 +0100

    Analyser ready for insights generation

--- classes/analytics/analyser/assign_submissions.php
+++ classes/analytics/analyser/assign_submissions.php
@@ -55,6 +55,9 @@ class assign_submissions extends by_activity {
     protected function cmid_from_sampleid($sampleid) {
         global $DB;
 
+        // TODO Add a request cache with 2 request-level arrays. One to list the
+        // cms we already fetched and another one, indexed by ass.id and with cm.id
+        // value.
         $sql = "SELECT cm.id FROM {course_modules} cm
                   JOIN {modules} m ON m.id = cm.module
                   JOIN {assign_submission} ass ON ass.assignment = cm.instance
@@ -62,6 +65,7 @@ class assign_submissions extends by_activity {
         $cm = $DB->get_record_sql($sql, ['id' => $sampleid]);
         return $cm->id;
     }
+
     /**
      * Returns the analysable of a sample
      *
@@ -111,10 +115,19 @@ class assign_submissions extends by_activity {
 
         $userids = array_keys($participants);
 
-        // TODO assign API.
         list($usersql, $userparams) = $DB->get_in_or_equal($userids, SQL_PARAMS_NAMED);
-        $params = $userparams + ['assign' => $cm->get_cm_info()->instance];
-        $submissions = $DB->get_records_select('assign_submission', "assignment = :assign AND userid $usersql", $params);
+        $params = $userparams + ['assign' => $cm->get_cm_info()->instance, 'latest' => 1];
+        $submissions = $DB->get_records_select('assign_submission', "assignment = :assign AND userid $usersql AND latest = :latest",
+          $params);
+
+        if (count($submissions) < count($participants)) {
+            // Create the missing assign_submission objects, even if they are empty.
+            foreach ($participants as $participant) {
+                if ($newsubmission = $assign->get_user_submission($participant->id, true)) {
+                    $submissions[$newsubmission->id] = $newsubmission;
+                }
+            }
+        }
 
         $enrolments = enrol_get_course_users($cm->get_cm_info()->get_course()->id, true, $userids);
         $uebyuserid = array_reduce($enrolments, function($carry, $ue) {
@@ -156,7 +169,6 @@ class assign_submissions extends by_activity {
     public function get_samples($sampleids) {
         global $DB;
 
-        // TODO assign API.
         list($sql, $params) = $DB->get_in_or_equal($sampleids, SQL_PARAMS_NAMED);
         $submissions = $DB->get_records_select('assign_submission', "id $sql", $params);
 
@@ -173,14 +185,15 @@ class assign_submissions extends by_activity {
         foreach ($submissions as $as) {
 
             if (empty($assigns[$as->assignment])) {
-                list($courses[$as->course], $cm) = get_course_and_cm_from_instance($as->assignment, 'assign', 0, -1);
-                $cms[$cm->id] = $cm;
-                $assigns[$as->assignment] = new \assign($cm->context, $cms[$cm->id], $course);
-                $participants[$as->assignment] = $assign->list_participants(0, false);
+                list($course, $cm) = get_course_and_cm_from_instance($as->assignment, 'assign', 0, -1);
+                $cms[$as->assignment] = $cm;
+                $courses[$cm->course] = $course;
+                $assigns[$as->assignment] = new \assign($cm->context, $cm, $course);
+                $participants[$as->assignment] = $assigns[$as->assignment]->list_participants(0, false);
 
-                if (empty($uebyuserid[$as->course])) {
-                    $enrolments = enrol_get_course_users($as->course, true, array_keys($participants[$as->assignment]));
-                    $uebyuserid[$as->course] = array_reduce($enrolments, function($carry, $ue) {
+                if (empty($uebyuserid[$cm->course])) {
+                    $enrolments = enrol_get_course_users($course->id, true, array_keys($participants[$as->assignment]));
+                    $uebyuserid[$course->id] = array_reduce($enrolments, function($carry, $ue) {
                         $carry[$ue->id] = (object)[
                             'id' => $ue->ueid,
                             'userid' => $ue->id,
@@ -197,13 +210,15 @@ class assign_submissions extends by_activity {
                 }
             }
 
+            $samplecm = $cms[$as->assignment];
+
             $samples[0][$as->id] = $as->id;
             $samples[1][$as->id] = array(
                 'course' => $assigns[$as->assignment]->get_course(),
                 'user' => $participants[$as->assignment][$as->userid],
-                'user_enrolments' => $uebyuserid[$as->course][$as->userid],
-                'context' => $cms[$as->assignment]->get_cm_info()->context,
-                'course_modules' => $cms[$as->assignment]->get_cm_info()->get_course_module_record(),
+                'user_enrolments' => $uebyuserid[$samplecm->course][$as->userid],
+                'context' => $samplecm->context,
+                'course_modules' => $cm->get_course_module_record(),
                 'assign' => $assigns[$as->assignment]->get_instance(),
                 'assign_submission' => $as
             );
@@ -221,8 +236,7 @@ class assign_submissions extends by_activity {
      * @return array array(string, \renderable)
      */
     public function sample_description($sampleid, $contextid, $sampledata) {
-        $description = format_string($sampledata['assign']->name, true, array('context' => $sampledata['context']));
-        $image = new \image_icon('icon', $description, 'assign');
-        return array($description, $image);
+        $description = fullname($sampledata['user'], true, array('context' => $contextid));
+        return array($description, new \user_picture($sampledata['user']));
     }
 }
